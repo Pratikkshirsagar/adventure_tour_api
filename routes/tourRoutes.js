263c420c903be6ecf4fb263b68eb2ccec0176c89
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-09 19:00:11 +0530

    file structure added for routing and controllers

---
+++ routes/tourRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { getAllTours, getTour, addTour, updateTour, deleteTour } = require(
+  "../controllers/tourController",
+);
+const router = express.Router();
+
+router.route("/").get(getAllTours).post(addTour);
+
+router
+  .route("/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+module.exports = router;
