a1acdd3989cd568ec93e281e521d900570f2e7f5
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-09 18:23:21 +0530

    spell corrected

--- server.js
+++ server.js
@@ -111,35 +111,35 @@ const deleteTour = (req, res) => {
 const getAllUsers = (req, res) => {
   res.status(500).json({
     status: "error",
-    message: "This route is not yet defines!",
+    message: "This route is not yet defined!",
   });
 };
 
 const createUser = (req, res) => {
   res.status(500).json({
     status: "error",
-    message: "This route is not yet defines!",
+    message: "This route is not yet defined!",
   });
 };
 
 const getUser = (req, res) => {
   res.status(500).json({
     status: "error",
-    message: "This route is not yet defines!",
+    message: "This route is not yet defined!",
   });
 };
 
 const updateUser = (req, res) => {
   res.status(500).json({
     status: "error",
-    message: "This route is not yet defines!",
+    message: "This route is not yet defined!",
   });
 };
 
 const deleteUser = (req, res) => {
   res.status(500).json({
     status: "error",
-    message: "This route is not yet defines!",
+    message: "This route is not yet defined!",
   });
 };
 
