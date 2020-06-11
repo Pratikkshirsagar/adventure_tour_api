de250034dd7bee83e5338bd68e3fbf7e8f46b46c
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-11 20:46:59 +0530

    more data fields added to tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5,
+    required: [true, 'A tour must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty'],
+  },
+  ratingsAvarage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: [true, 'A tour must have a summery'],
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
