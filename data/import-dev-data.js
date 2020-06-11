4832bfaa683fa6330236b89dec5bd82341290063
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-11 22:40:43 +0530

    import local data to db script done

---
+++ data/import-dev-data.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const Tours = require('../models/tourModel');
+const mongoose = require('mongoose');
+
+//Load env vars
+dotenv.config({ path: '../config/config.env' });
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, `utf-8`)
+);
+
+// IMPORT ALL DATA TO DB
+const importData = async () => {
+  try {
+    await Tours.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tours.deleteMany();
+    console.log('Data Successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--i') {
+  importData();
+} else if (process.argv[2] === '--d') {
+  deleteData();
+}
