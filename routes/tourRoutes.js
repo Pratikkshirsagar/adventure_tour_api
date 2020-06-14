5342a97a99a72728145ed5dddf5fd5fe8a28d037
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-14 22:38:26 +0530

    monthly plan added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,9 +6,14 @@ const {
   updateTour,
   deleteTour,
   aliasTopTour,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
+
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/top-5-cheap').get(aliasTopTour, getAllTours);
 
