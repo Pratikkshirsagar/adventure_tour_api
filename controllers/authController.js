e51a234fb38a4fc7021004f448400f2f34f7dffd
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-17 15:30:53 +0530

    route protection done for valid users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const { find } = require('../models/userModel');
+const { decode } = require('punycode');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -49,4 +50,53 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token  and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You  are not logged in! Please log in to get access', 401)
+    );
+  }
+
+  // 2) Validate the token
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+  } catch (error) {
+    console.log(next(new AppError(`Not autherize to access this route`, 401)));
+  }
+
+  // 3) check if user still exist
+  const freshUser = await User.findById(decoded.id);
+
+  if (!freshUser) {
+    return next(
+      new AppError(`The token belonging to this user no longer exist.`, 401)
+    );
+  }
+
+  // 4) check if user change the password after the token was issued
+  if (freshUser.changedPasswordsAfter(decoded.iat)) {
+    next(
+      new AppError(
+        `User recently changed the password! Please login again.`,
+        401
+      )
+    );
+  }
+
+  req.user = freshUser;
+  next();
 });
