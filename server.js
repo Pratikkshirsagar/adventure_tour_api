98934a84260a7b0cc5fa52401c14c8a5f93065a3
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-21 21:41:08 +0530

    data sanitization done

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const colors = require('colors');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./utils/err');
@@ -24,6 +26,13 @@ const app = express();
 // body parser
 app.use(express.json({ limit: '10kb' }));
 
+// data sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
+// loging the api
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
