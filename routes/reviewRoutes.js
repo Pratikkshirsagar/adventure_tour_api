f39acaa6ca27cd6d3e424016ccf429f2a124103b
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-22 18:21:45 +0530

    crud operations done for reviews

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {
+  getAllReview,
+  getReview,
+  createReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviewController');
+
+const router = express.Router();
+
+router.route('/').get(getAllReview).post(createReview);
+
+router.route('/:id').get(getReview).patch(updateReview).delete(deleteReview);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./utils/err');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -78,6 +79,7 @@ app.use((req, res, next) => {
 // Router
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
