05c77139c9fdff749e14eb098738bb8ee71059a3
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-23 01:01:26 +0530

    indexes added

--- data/import-dev-data.js
+++ data/import-dev-data.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
-const Tours = require('../models/tourModel');
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
 const mongoose = require('mongoose');
 
 //Load env vars
@@ -14,11 +16,17 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, `utf-8`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, `utf-8`));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, `utf-8`)
+);
 
 // IMPORT ALL DATA TO DB
 const importData = async () => {
   try {
-    await Tours.create(tours);
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded');
   } catch (error) {
     console.log(error);
@@ -29,7 +37,9 @@ const importData = async () => {
 // DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
-    await Tours.deleteMany();
+    await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data Successfully deleted');
   } catch (error) {
     console.log(error);
